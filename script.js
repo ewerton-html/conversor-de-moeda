848709278133697a373707ae8304339038055125
Ewerton Ribeiro da Silva <ewerton.silvatv96@gmail.com>
2021-09-27 10:24:44 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function dolar() {
+  criarConvercao(converter("dolar"));
+}
+
+function euro() {
+  criarConvercao(converter("euro"));
+}
+
+function bitcoin() {
+  criarConvercao(converter("bitcoin"));
+}
+
+function criarConvercao(tipoConversor) {
+  var localConvercao = document.getElementById("localConvercao");
+  var campo = "<input type='number' id='valor' size='2' /><br>";
+  var botao =
+    "<input type='submit' onclick='" + tipoConversor + "' value='Converter' />";
+  localConvercao.innerHTML = campo + botao;
+  console.log(localConvercao);
+}
+
+function converter(moeda) {
+  var tipoConversor = "";
+  if (moeda == "dolar") {
+    tipoConversor = "conversorDolar()";
+  } else if (moeda == "euro") {
+    tipoConversor = "conversorEuro()";
+  } else {
+    tipoConversor = "conversorBitCoin()";
+  }
+  return tipoConversor;
+}
+
+function conversorDolar() {
+  var localValor = document.getElementById("valorConvertido");
+  var pegandoValor = document.getElementById("valor").value;
+  var convertendoValor = pegandoValor * 5.34;
+  convertendoValor = parseInt(convertendoValor);
+  var mostrandoValor = "O valor é R$:" + convertendoValor;
+  localValor.innerHTML = mostrandoValor;
+}
+
+function conversorEuro() {
+  var localValor = document.getElementById("valorConvertido");
+  var pegandoValor = document.getElementById("valor").value;
+  var convertendoValor = pegandoValor * 6.25;
+  convertendoValor = parseInt(convertendoValor);
+  var mostrandoValor = "O valor é R$:" + convertendoValor;
+  localValor.innerHTML = mostrandoValor;
+}
+
+function conversorBitCoin() {
+  var localValor = document.getElementById("valorConvertido");
+  var pegandoValor = document.getElementById("valor").value;
+  var convertendoValor = pegandoValor * 233169.03;
+  var mostrandoValor = "O valor é R$:" + convertendoValor;
+  localValor.innerHTML = mostrandoValor;
+}
